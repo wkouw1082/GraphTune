a622b859003d52b872711c051094377e94c8eab8
tyokoyama <s203193@stn.nagaokaut.ac.jp>
2022-05-05 21:10:29 +0900

    2-tuplesのDFSコードを生成するように前処理機能を追加

--- preprocess.py
+++ preprocess.py
@@ -49,6 +49,71 @@ def preprocess(params, train_directory='./dataset/train/', valid_directory='./da
     get_onehot_and_list(valid_dfs, time_dict,node_dict, max_sequence_length, valid_label, valid_directory, params.ignore_label)
     get_onehot_and_list(test_dfs,  time_dict,node_dict, max_sequence_length, test_label,  test_directory,  params.ignore_label)
 
+
+def preprocess_for_2_tuples(params, train_directory='./dataset/train/', valid_directory='./dataset/valid/',
+               test_directory='./dataset/test/'):
+    """
+    生データセットからグラフオブジェクト(2-tuples)を作成し、それとそのサイズを保存する関数.
+
+        Args:
+            params (config.Parameters)  : グローバル変数のセット
+            train_directory      (str)  : 前処理後のtrain用データセットが保存されるディレクトリ
+                default: './dataset/train/'
+            valid_directory      (str)  : 前処理後のvalidation用データセットが保存されるディレクトリ
+                default: './dataset/valid/'
+            test_directory       (str)  : 前処理後のtest用データセットが保存されるディレクトリ
+                default: './dataset/test/'
+    """
+
+    complex_network = complex_networks.ComplexNetworks()
+    complex_network.make_twitter_graph_with_label()
+
+    train_dfs, train_time_set, train_node_set, train_max_length, train_label = to_dfs_conditional(
+        params.train_network_detail, params.dfs_mode)
+    valid_dfs, valid_time_set, valid_node_set, valid_max_length, valid_label = to_dfs_conditional(
+        params.valid_network_detail, params.dfs_mode)
+    test_dfs, test_time_set, test_node_set, test_max_length, test_label = to_dfs_conditional(
+        params.test_network_detail, params.dfs_mode)
+
+    # label normalization
+    if params.split_size["test"] == 0:
+        label_data_list = [train_label, valid_label]
+    else:
+        label_data_list = [train_label, valid_label, test_label]
+    for label_data in label_data_list:
+        # Search max, min
+        max_val, min_val = -1, 10000
+        for i, label in enumerate(label_data):
+            if max_val < label.item():
+                max_val = label.item()
+            if min_val > label.item():
+                min_val = label.item()
+        # Normalization
+        for i, label in enumerate(label_data):
+            label_data[i] = (label - min_val) / (max_val - min_val)
+
+    time_stamp_set = train_time_set | valid_time_set | test_time_set
+    node_label_set = train_node_set | valid_node_set | test_node_set
+    max_sequence_length = max(train_max_length, valid_max_length, test_max_length)
+    conditional_label_length = params.condition_size  # 指定しているパラメータの数
+
+    print(f"max_sequence_length = {max_sequence_length}")
+
+    joblib.dump([len(time_stamp_set) + 1, len(node_label_set) + 1, 2, conditional_label_length], "dataset/param")
+
+    time_dict = {time: index for index, time in enumerate(time_stamp_set)}
+    node_dict = {node: index for index, node in enumerate(node_label_set)}
+
+    del time_stamp_set, node_label_set
+
+    get_onehot_and_list_2_tuples(train_dfs, time_dict, node_dict, max_sequence_length, train_label, train_directory,
+                        params.ignore_label)
+    get_onehot_and_list_2_tuples(valid_dfs, time_dict, node_dict, max_sequence_length, valid_label, valid_directory,
+                        params.ignore_label)
+    if not params.split_size["test"] == 0:
+        get_onehot_and_list_2_tuples(test_dfs, time_dict, node_dict, max_sequence_length, test_label, test_directory,
+                        params.ignore_label)
+
 def to_dfs_conditional(detail, dfs_mode):
     """
     labelが添付されたDFSコードを生成する関数
@@ -159,6 +224,48 @@ def get_onehot_and_list(dfs_code, time_dict, node_dict, max_sequence_length, lab
     joblib.dump([t_u_list,t_v_list,n_u_list,n_v_list,e_list],directory+'label')
     joblib.dump(label_set,directory+'conditional')
 
+def get_onehot_and_list_2_tuples(dfs_code, time_dict, node_dict, max_sequence_length, label_set, directory, ignore_label):
+    """指定されたサイズのone-hotへ変換する関数(2-tuples)
+
+    Args:
+        dfs_code (_type_): _description_
+        time_dict (_type_): _description_
+        node_dict (_type_): _description_
+        max_sequence_length (_type_): _description_
+        label_set (_type_): _description_
+        directory (_type_): _description_
+        ignore_label (int): 2-tuples内で無視するラベル
+    """
+    time_end_num = len(time_dict.keys())
+    dfs_code_onehot_list = []
+    t_u_list = []
+    t_v_list = []
+    for data in dfs_code:
+        data = data.T
+        # IDに振りなおす
+        t_u = [time_dict[t] for t in data[0]]
+        t_u.append(time_end_num)
+        t_u = np.array(t_u)
+        t_u_list.append(t_u)
+        t_v = [time_dict[t] for t in data[1]]
+        t_v.append(time_end_num)
+        t_v = np.array(t_v)
+        t_v_list.append(t_v)
+
+        onehot_t_u = utils.convert2onehot(t_u, time_end_num + 1)
+        onehot_t_v = utils.convert2onehot(t_v, time_end_num + 1)
+
+        dfs_code_onehot_list.append(np.concatenate([onehot_t_u, onehot_t_v], 1))
+
+    dfs_code_onehot_list = torch.Tensor(utils.padding(dfs_code_onehot_list, max_sequence_length, 0))
+    t_u_list = torch.LongTensor(utils.padding(t_u_list, max_sequence_length, ignore_label))
+    t_v_list = torch.LongTensor(utils.padding(t_v_list, max_sequence_length, ignore_label))
+
+    joblib.dump(dfs_code_onehot_list, directory + 'onehot_2_tuples')
+    joblib.dump([t_u_list, t_v_list], directory + 'label_2_tuples')
+    joblib.dump(label_set, directory + 'conditional_2_tuples')
 
 if __name__ == "__main__":
-    preprocess(train_network_detail, valid_network_detail)
+    from config import Parameters
+    params = Parameters()
+    preprocess(params, params.train_network_detail, params.valid_netwosrk_detail, params.test_network_detail)
